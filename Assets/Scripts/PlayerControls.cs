0500a5e979dd6092356e10cd2655a41769278687
Bryan Blaze <100726382+BryanBlaze00@users.noreply.github.com>
2023-11-16 18:49:43 -0700

    minor changes

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
+    [SerializeField] InputAction movement;
 
     void Start()
     {
@@ -12,10 +14,10 @@ public class PlayerControls : MonoBehaviour
 
     void Update()
     {
-        float horizontalMove = Input.GetAxis("Horizontal");
-        float verticalalMove = Input.GetAxis("Vertical");
+        // float horizontalMove = Input.GetAxis("Horizontal");
+        // float verticalalMove = Input.GetAxis("Vertical");
 
-        Debug.Log(horizontalMove);
-        Debug.Log(verticalalMove);
+        // Debug.Log(horizontalMove);
+        // Debug.Log(verticalalMove);
     }
 }
